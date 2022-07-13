e6d208c07999aca043f027737333765fae790618
Lu Ji <lu.ji1@me.com>
2022-07-13 14:08:48 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './useTimeout';
+export * from "./useDebounce";
+export * from './useArray';
+export * from './useChannel';
+export * from './useCountdown';
---
+++ useChannel.ts
@@ -0,0 +1,36 @@
+import { Channel } from "phoenix";
+import { useEffect, useState } from "react";
+import socket from "../user_socket";
+
+export type Callback = ((data: any) => void) | null;
+type ChannelInitFn = (channel: Channel) => void;
+
+/**
+ * Sets up a Phoenix Channel 
+ * @param topic 
+ * @param successCallback 
+ * @param errorCallback 
+ * @param channelInit For setting up channel message handlers 
+ * @returns 
+ */
+export function useChannel(topic: string, successCallback?: Callback, errorCallback?: Callback, channelInit?: ChannelInitFn) {
+  const [channel, setChannel] = useState<Channel>();
+
+  useEffect(() => {
+    const channel = socket.channel(topic);
+    channel
+      .join()
+      .receive('ok', (data) => successCallback && successCallback(data))
+      .receive('error', (resp) => errorCallback ? errorCallback(resp) : console.error('Unable to join', resp));
+
+    channelInit && channelInit(channel);
+
+    setChannel(channel);
+
+    return () => {
+      channel.leave();
+    }
+  }, []);
+
+  return channel;
+}
